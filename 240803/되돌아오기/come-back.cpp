26193d9c8183d773c530917d32651674c086df18
최은별 <99008137+dmsquf03@users.noreply.github.com>
2024-08-03 21:06:12 +0900

    Title: 되돌아오기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240803/되돌아오기/come-back.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+using namespace std;
+
+int GetDir(char c){
+    if(c == 'W') return 0;
+    else if(c == 'S') return 1;
+    else if(c == 'N') return 2;
+    else return 3;
+}
+
+bool IsStart(int x, int y){
+    return (x == 0 && y == 0);
+}
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n, x = 0, y = 0, dir, move, t = 0, flag = 0;
+    int dx[4] = {-1, 0, 0, 1}, dy[4] = {0, -1, 1, 0}; // w s n e
+    char c;
+
+    // 입력
+    cin >> n;
+
+    // 실행
+    while(n--){
+        cin >> c >> move;
+        dir = GetDir(c);
+        while(move--){
+            t++;
+            x += dx[dir];
+            y += dy[dir];
+
+            if(IsStart(x, y)){
+                flag = 1;
+                break;
+            }
+        }
+        if(flag == 1) break;
+    }
+
+    if(flag == 0) t = -1;
+
+    // 출력
+    cout << t;
+
+    return 0;
+}
