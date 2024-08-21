848b0c3c2be2390cf4b91649e6a4f47b15ab9869
최은별 <99008137+dmsquf03@users.noreply.github.com>
2024-08-21 18:30:02 +0900

    Title: 최고의 13위치, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240821/최고의 13위치/best-place-of-13.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int n, max_v = 0;
+vector<vector<int>> arr;
+
+void findMaxCoin(){
+    int cnt;
+    for(int i = 0; i < n - 2; i++){
+        cnt = 0;
+        for(int j = 0; j < n; j++){
+            cnt = arr[i][j] + arr[i][j + 1] + arr[i][j + 2];
+            max_v = max(max_v, cnt);
+        }
+    }
+}
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    // 입력
+    cin >> n;
+    arr = vector<vector<int>> (n, vector<int>(n, 0));
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < n; j++){
+            cin >> arr[i][j];
+        }
+    }
+
+    // 실행
+    findMaxCoin();
+
+    // 출력
+    cout << max_v;
+    return 0;
+}
