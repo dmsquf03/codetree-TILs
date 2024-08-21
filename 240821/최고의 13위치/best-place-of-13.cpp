9200b5989a8a7dbfd5252ee3893008ea8cd04482
최은별 <99008137+dmsquf03@users.noreply.github.com>
2024-08-21 18:33:55 +0900

    Title: 최고의 13위치, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240821/최고의 13위치/best-place-of-13.cpp
+++ 240821/최고의 13위치/best-place-of-13.cpp
@@ -8,11 +8,11 @@ vector<vector<int>> arr;
 
 void findMaxCoin(){
     int cnt;
-    for(int i = 0; i < n - 2; i++){
-        for(int j = 0; j < n; j++){
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < n - 2; j++){
             cnt = arr[i][j] + arr[i][j + 1] + arr[i][j + 2];
             max_v = max(max_v, cnt);
-            cout << max_v <<endl;
+            // cout << max_v << endl;
         }
     }
 }
