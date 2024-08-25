5126e4db3a11f70e6d961e23826e67af5d3d506b
최은별 <99008137+dmsquf03@users.noreply.github.com>
2024-08-25 22:05:47 +0900

    Title: 체크판위에서 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240825/체크판위에서 2/on-the-checkboard-2.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int r, c, result = 0;
+vector<vector<char>> pan;
+
+void Find4MoveAbleCnt(){
+    int cr = 0, cc = 0, nr, nc;
+    for(int i = cr + 1; i < r - 2; i++){
+        for(int j = cc + 1; j < c - 2; j++){
+            if(pan[0][0] == pan[i][j]){
+                continue;
+            }
+            for(int k = i + 1; k < r - 1; k++){
+                for(int l = j + 1; l < c - 1; l++){
+                    if(pan[i][j] != pan[k][l] && pan[k][l] != pan[r - 1][c - 1]){
+                        result++;
+                    }
+                }
+            }
+        }
+    }
+}
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    // 입력
+    cin >> r >> c;
+
+    pan = vector<vector<char>> (r, vector<char>(c));
+
+    for(int i = 0; i < r; i++){
+        for(int j = 0; j < c; j++){
+            cin >> pan[i][j];
+        }
+    }
+
+    // 실행
+    Find4MoveAbleCnt();
+
+    // 출력
+    cout << result;
+
+    return 0;
+}
