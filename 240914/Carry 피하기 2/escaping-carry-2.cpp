c0ebcc4d486c923b47dd357b0692b0b3e6bf8254
최은별 <99008137+dmsquf03@users.noreply.github.com>
2024-09-14 01:06:25 +0900

    Title: Carry 피하기 2, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240914/Carry 피하기 2/escaping-carry-2.cpp
+++ 240914/Carry 피하기 2/escaping-carry-2.cpp
@@ -2,10 +2,9 @@
 #include <vector>
 #include <string>
 #include <algorithm>
-#include <tuple>
 using namespace std;
 
-vector<string> nums;
+vector<int> nums;
 int max_v = 0, n;
 
 void FindNoCarryMax(){
@@ -13,41 +12,31 @@ void FindNoCarryMax(){
     // 각 자리별 숫자 더하기 10 넘으면 pass
     // 아니면 3개 다 더한 값 sum에 저장
     // max_v와 sum 비교 후 갱신
-    int t_sum, sum, flag;
-    vector<pair<int, string>> num_lens;
-    for(int i = 0; i < n - 2; i++){// 1번째
-        for(int j = i + 1; j < n - 1; j++){// 2번째
-            for(int k = j + 1; k < n; k++){// 3번째
-                num_lens = vector<pair<int, string>>(); // 숫자, 숫자 길이
-                num_lens.emplace_back(nums[i].length(), nums[i]);
-                num_lens.emplace_back(nums[j].length(), nums[j]);
-                num_lens.emplace_back(nums[k].length(), nums[k]);
-                sort(num_lens.begin(), num_lens.end()); // 오름차순 정리
-                flag = 1;
-                // 1의 자리부터, 최대 자리까지
-                for(int l = 0; l < num_lens[2].first; l++){// 자리별 숫자 10 넘으면 break
-                    if(l < num_lens[0].first){// 다 더해야
-                        sum = (num_lens[0].second[num_lens[0].first-(l+1)] - '0') + (num_lens[1].second[num_lens[1].first-(l+1)] - '0') + (num_lens[2].second[num_lens[2].first-(l+1)] - '0');
-                        if(sum >= 10){
-                            flag = 0;
-                            break;
-                        }
-                    }
-                    else if(l >= num_lens[0].first && l < num_lens[1].first){// 작은 숫자 빼고 
-                        sum = (num_lens[1].second[num_lens[1].first-(l+1)] - '0') + (num_lens[2].second[num_lens[2].first-(l+1)] - '0');
-                        if(sum >= 10){
-                            flag = 0;
-                            break;
-                        }
-                    }
-                }
-                if(flag){
-                    t_sum = stoi(nums[i]) + stoi(nums[j]) + stoi(nums[k]);
-                    max_v = max(max_v, t_sum);
-                }
-            }
-        }
-    }
+    int ans = -1;
+    for(int i = 0; i < n - 2; i++)
+        for(int j = i + 1; j < n - 1; j++)
+            for(int k = j + 1; k < n; k++) {
+				bool carry = false;
+				
+				//일의 자리에서 carry가 발생하는 경우
+                if(nums[i] % 10 + nums[j] % 10 + nums[k] % 10 >= 10)
+					carry = true;
+				
+				//십의 자리에서 carry가 발생하는 경우
+                if(nums[i] % 100 / 10 + nums[j] % 100 / 10 + nums[k] % 100 / 10 >= 10)
+					carry = true;
+				
+				//백의 자리에서 carry가 발생하는 경우
+                if(nums[i] % 1000 / 100 + nums[j] % 1000 / 100 + nums[k] % 1000 / 100 >= 10)
+					carry = true;
+				
+				//천의 자리에서 carry가 발생하는 경우
+                if(nums[i] % 10000 / 1000 + nums[j] % 10000 / 1000 + nums[k] % 10000 / 1000 >= 10)
+					carry = true;
+				
+				if(carry == false)
+					max_v = max(max_v, nums[i] + nums[j] + nums[k]);
+			}
 }
 
 int main() {
@@ -63,9 +52,6 @@ int main() {
     FindNoCarryMax();
 
     // 출력
-    if(max_v == 0){
-        cout << "-1";
-    }
-    else cout << max_v;
+    cout << max_v;
     return 0;
 }
