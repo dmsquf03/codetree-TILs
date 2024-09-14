2a9e45c1185f7ea87a8677af7fc6ea2e1634a9f9
최은별 <99008137+dmsquf03@users.noreply.github.com>
2024-09-14 22:55:46 +0900

    Title: 오목, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240914/오목/O-mok.cpp
+++ 240914/오목/O-mok.cpp
@@ -6,18 +6,19 @@ int winner = 0, x = 0, y = 0;
 vector<vector<int>> pan(19, vector<int>(19, 0));
 
 void FindWinner(){
-    int winx, winy, wind;
+    int winx, winy, windl, windr;
     for(int i = 2; i < 17; i++){ // 가운데 기준
         for(int j = 2; j < 17; j++){
             winx = pan[i - 2][j] * pan[i - 1][j] * pan[i][j] * pan[i + 1][j] * pan[i + 2][j];
             winy = pan[i][j - 2] * pan[i][j - 1] * pan[i][j] * pan[i][j + 1] * pan[i][j + 2];
-            wind = pan[i - 2][j - 2] * pan[i - 1][j - 1] * pan[i][j] * pan[i + 1][j + 1] * pan[i + 2][j + 2];
-            if(winx == 1 || winy == 1 || wind == 1){
+            windl = pan[i - 2][j - 2] * pan[i - 1][j - 1] * pan[i][j] * pan[i + 1][j + 1] * pan[i + 2][j + 2];
+            windr = pan[i - 2][j + 2] * pan[i - 1][j + 1] * pan[i][j] * pan[i + 1][j - 1] * pan[i + 2][j - 2];
+            if(winx == 1 || winy == 1 || windl == 1 || windr == 1){
                 winner = 1;
                 x = i;
                 y = j;
             }
-            else if(winx == 32 || winy == 32 || wind == 32){
+            else if(winx == 32 || winy == 32 || windl == 32 || windr == 32){
                 winner = 2;
                 x = i;
                 y = j;
