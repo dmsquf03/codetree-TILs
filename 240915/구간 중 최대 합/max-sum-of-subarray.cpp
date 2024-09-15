7c9c02323e129e9aef79c11f786e00ae18e7162c
최은별 <99008137+dmsquf03@users.noreply.github.com>
2024-09-15 13:03:27 +0900

    Title: 구간 중 최대 합, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240915/구간 중 최대 합/max-sum-of-subarray.cpp
+++ 240915/구간 중 최대 합/max-sum-of-subarray.cpp
@@ -8,7 +8,7 @@ vector<int> nums;
 
 void FindMaxSum(){
     int sum;
-    for(int i = 0; i < n - k; i++){// 시작
+    for(int i = 0; i < n - k + 1; i++){// 시작
         sum = 0;
         for(int j = 0; j < k; j++){// 합
             sum += nums[i + j];
