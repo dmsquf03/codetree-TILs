7296c752a18edb69c17ecd78b4d3b2f01481757c
최은별 <99008137+dmsquf03@users.noreply.github.com>
2024-09-16 13:35:03 +0900

    Title: 최소공배수 구하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240916/최소공배수 구하기/find-the-least-common-multiple.cpp
+++ 240916/최소공배수 구하기/find-the-least-common-multiple.cpp
@@ -5,32 +5,13 @@
 using namespace std;
 
 void FindLCM(int n, int m){
-    // 분해
-    int lcm = 1, cn = n, cm = m;
-    vector<int> a(100, 0);
-    vector<int> b(100, 0);
-
-    for(int i = 2; i < n; i++){
-        if(cn % i == 0){
-            a[i]++;
-            cn /= i;
-            i--;
+    int lcm, cnt = 1;
+    while(1){
+        if(n * cnt % m == 0){
+            lcm = n * cnt;
+            break;
         }
-    }
-    for(int i = 2; i < m; i++){
-        if(cm % i == 0){
-            b[i]++;
-            cm /= i;
-            i--;
-        }
-    }
-
-    // lcm 구하기
-    int c;
-    for(int i = 2; i < 100; i++){
-        if(a[i] == 0 && b[i] == 0) continue;
-        c = pow(i, max(a[i], b[i]));
-        lcm *= c;
+        cnt++;
     }
     cout << lcm;
 }
