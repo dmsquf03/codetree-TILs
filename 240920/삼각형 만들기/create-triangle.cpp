4ff96fefad9ed84df8c23bc796a3ff7fbd4467b5
최은별 <99008137+dmsquf03@users.noreply.github.com>
2024-09-20 20:42:53 +0900

    Title: 삼각형 만들기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240920/삼각형 만들기/create-triangle.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <vector>
+#include <climits>
+#include <algorithm>
+#include <tuple>
+#include <cstdlib>
+using namespace std;
+
+int n;
+double result = 0;
+vector<pair<int, int>> point;
+
+void GetMaxAreaDouble(){
+    double area;
+    for(int i = 0; i < n; i++){// 1번째
+        for(int j = 0; j < n; j++){// 2번째
+            if(j == i) continue;
+            for(int k = 0; k < n; k++){// 3번째 (1번과 x좌표 == 2번과 y좌표 ==)
+                if(k == i || k == j) continue;
+                if(point[k].first != point[i].first || point[k].second != point[j].second) continue;
+                area = abs(point[k].second - point[i].second) * abs(point[k].first - point[j].first);
+                result = max(result, area);
+            }
+        }
+    }
+}
+
+int main() {
+    // 입력
+    cin >> n;
+    
+    int x, y;
+    for(int i = 0; i < n; i++){
+        cin >> x >> y;
+        point.emplace_back(x, y);
+    }
+
+    // 실행
+    GetMaxAreaDouble();
+
+    // 출력
+    cout << result;
+    return 0;
+}
