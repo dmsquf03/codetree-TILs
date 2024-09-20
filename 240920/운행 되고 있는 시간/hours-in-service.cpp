f777e5c6d49cc7bf07a9b3ce539fb313b096daf9
최은별 <99008137+dmsquf03@users.noreply.github.com>
2024-09-20 21:41:51 +0900

    Title: 운행 되고 있는 시간, Time: 17ms, Memory: 0MB, Status: Passed - CodeTree

--- 240920/운행 되고 있는 시간/hours-in-service.cpp
+++ 240920/운행 되고 있는 시간/hours-in-service.cpp
@@ -7,12 +7,11 @@ using namespace std;
 int n, result = 0;
 vector<pair<int, int>> work;
 
-void GetMaxRunTime(){
-    int time[1001] = {0};
+void GetMaxRunTime(){;
     int t_time;
 
     for(int i = 0; i < n; i++){
-        time[1001] = {0};
+        int time[1001] = {0};
         for(int j = 0; j < n; j++){
             if(j == i) continue;
             for(int k = work[j].first; k < work[j].second; k++){
@@ -22,7 +21,7 @@ void GetMaxRunTime(){
             for(int k = 1; k < 1001; k++){
                 t_time += time[k];
             }
-            result = max(result, t_time) - 1;
+            result = max(result, t_time);
         }
     }
 }
