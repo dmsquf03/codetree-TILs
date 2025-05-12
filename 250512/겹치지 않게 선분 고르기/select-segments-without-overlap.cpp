1befd1a3f7262188eabb714be6e149f65ffedf01
최은별 <99008137+dmsquf03@users.noreply.github.com>
2025-05-12 18:47:07 +0900

    Title: 겹치지 않게 선분 고르기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 250512/겹치지 않게 선분 고르기/select-segments-without-overlap.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <vector>
+#include <utility>
+#include <cmath>
+
+using namespace std;
+
+int n, result = 0;
+vector<pair<int, int>> lines;
+vector<int> selected;
+
+bool IsSelectable(int idx){ // idx번째 선분을 추가로 선택할 수 있는지 검사하는 함수.
+    bool valid = true, condition;
+    for(int i = 0; i < selected.size(); i++){
+        condition = lines[idx].first > lines[i].second || lines[idx].second < lines[i].first;
+        if(!condition){
+            valid = false;
+            break;
+        }
+    }
+    return valid;
+}
+
+void Choose(int curr_num){  // curr_num번째 선분 선택 결정 함수.
+    if(curr_num == n){  // 마지막 선분까지 결정 완료
+        int cnt = selected.size();
+        result = max(result, cnt);
+        return;
+    }
+    if(IsSelectable(curr_num)){ // 선택 가능할 때 분기 진행
+        selected.push_back(curr_num);
+        Choose(curr_num + 1);
+        selected.pop_back(); 
+    }
+    Choose(curr_num + 1);
+
+    return;
+}
+
+int main() {
+    int l, r;
+    cin >> n;
+
+    for(int i = 0; i < n; i++){
+        cin >> l >> r;
+        lines.push_back({l, r});
+    }
+
+    Choose(0);
+
+    cout << result;
+
+    return 0;
+}
